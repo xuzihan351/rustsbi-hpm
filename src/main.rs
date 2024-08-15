2b218a149e166e863dc488fe6d92dfacaf1d4446
Zihan Xu <zihan.xu@hpmicro.com>
2024-08-15 13:44:31 +0800

    test for clear DC WAROUND
    Signed-off-by: Zihan Xu <zihan.xu@hpmicro.com>

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,7 @@ fn main() -> ! {
         asm!("csrw mideleg,    {}", in(reg) !0);
         asm!("csrw medeleg,    {}", in(reg) !0);
         asm!("csrw mcounteren, {}", in(reg) !0);
+        asm!("csrc 0x7ca, {}", in(reg) 0x6000);
         asm!("csrs 0x7ca, {}", in(reg) 0x22);
         use riscv::register::{medeleg, mtvec};
         medeleg::clear_supervisor_env_call();
